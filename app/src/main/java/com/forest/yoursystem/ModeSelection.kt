15889100acb526aa360eae09b0f0658a84351e59
Kirill_Boyarshinov <skynet0887@gmail.com>
2022-03-02 03:48:53 +0700

    start

---
+++ app/src/main/java/com/forest/yoursystem/ModeSelection.kt
@@ -0,0 +1,11 @@
+package com.forest.yoursystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModeSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mode_selection)
+    }
+}
---
+++ app/src/main/java/com/forest/yoursystem/WelcomeScreen.kt
@@ -0,0 +1,15 @@
+package com.forest.yoursystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeScreen : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_screen)
+
+    }
+}
