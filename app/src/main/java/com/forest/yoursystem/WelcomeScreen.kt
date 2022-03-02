971ea2d407a637ddb7c957bdf9ff630b655155d8
Kirill_Boyarshinov <skynet0887@gmail.com>
2022-03-03 02:47:33 +0700

    "Экран приветствия систематизирован"

--- app/src/main/java/com/forest/yoursystem/WelcomeScreen.kt
+++ app/src/main/java/com/forest/yoursystem/WelcomeScreen.kt
@@ -2,14 +2,22 @@ package com.forest.yoursystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class WelcomeScreen : AppCompatActivity() {
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen)
 
+
+        val text = findViewById<TextView>(R.id.greetings)
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            //text.text = "Ты нажал на кнопку. Ура."
+            //println("test------------------------------------------")
+            setContentView(R.layout.mode_selection)
+
+         }
     }
 }
