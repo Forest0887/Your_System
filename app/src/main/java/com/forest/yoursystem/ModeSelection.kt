d47b959ab3231447563c2fe3cd126407aea5f4c9
Kirill_Boyarshinov <skynet0887@gmail.com>
2022-03-03 05:12:56 +0700

    Добавлен экран главного меню

--- app/src/main/java/com/forest/yoursystem/ModeSelection.kt
+++ app/src/main/java/com/forest/yoursystem/ModeSelection.kt
@@ -9,11 +9,5 @@ class ModeSelection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mode_selection)
-
-        val btnOffline = findViewById<Button>(R.id.btnOffline)
-        val text = findViewById<TextView>(R.id.testText)
-        btnOffline.setOnClickListener {
-            text.text = "Ты нажал на кнопку. Ура."
-        }
     }
 }
