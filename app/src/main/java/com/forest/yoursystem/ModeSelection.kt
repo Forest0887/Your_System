f612e64b26ae8a20a300118b5cb37116451fe1dc
Kirill_Boyarshinov <skynet0887@gmail.com>
2022-03-03 04:17:22 +0700

    Добавлен переход между Activity

--- app/src/main/java/com/forest/yoursystem/ModeSelection.kt
+++ app/src/main/java/com/forest/yoursystem/ModeSelection.kt
@@ -2,10 +2,18 @@ package com.forest.yoursystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ModeSelection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mode_selection)
+
+        val btnOffline = findViewById<Button>(R.id.btnOffline)
+        val text = findViewById<TextView>(R.id.testText)
+        btnOffline.setOnClickListener {
+            text.text = "Ты нажал на кнопку. Ура."
+        }
     }
 }
--- app/src/main/java/com/forest/yoursystem/WelcomeScreen.kt
+++ app/src/main/java/com/forest/yoursystem/WelcomeScreen.kt
@@ -1,23 +1,20 @@
 package com.forest.yoursystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen)
 
-
-        val text = findViewById<TextView>(R.id.greetings)
+        // Переход между разными Activity
         val btnStart = findViewById<Button>(R.id.btnStart)
         btnStart.setOnClickListener {
-            //text.text = "Ты нажал на кнопку. Ура."
-            //println("test------------------------------------------")
-            setContentView(R.layout.mode_selection)
-
+            val startNewActivity = Intent(this, ModeSelection::class.java)
+            startActivity(startNewActivity)
          }
     }
 }
