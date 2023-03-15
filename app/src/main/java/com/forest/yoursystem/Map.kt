931279c6a389c36df601499c8b6f329a709cd4d1
skynet <skynet0887@gmail.com>
2023-03-16 01:42:27 +0700

    Закончена работа c расписанием и прогресс баром

--- app/src/main/java/com/forest/yoursystem/Map.kt
+++ app/src/main/java/com/forest/yoursystem/Map.kt
@@ -1,17 +1,28 @@
 package com.forest.yoursystem
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.directions.driving.DrivingRoute
+import com.yandex.mapkit.directions.driving.DrivingSession
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObject
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.Error
+import com.yandex.runtime.image.ImageProvider
 
-class Map : AppCompatActivity() {
+
+class Map : AppCompatActivity(), DrivingSession.DrivingRouteListener {
     lateinit var mapView: MapView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +31,53 @@ class Map : AppCompatActivity() {
         setContentView(R.layout.map)
         mapView = findViewById(R.id.mapview)
         mapView.map.move(CameraPosition(Point(55.994446, 92.797586), 11.0f, 0.0f, 0.0f),
-        Animation(Animation.Type.SMOOTH, 300f), null)
+        Animation(Animation.Type.SMOOTH, 1f), null)
+        val mapKit: MapKit = MapKitFactory.getInstance()
+        val probki = mapKit.createTrafficLayer(mapView.mapWindow)
+        probki.isTrafficVisible = true
+
+        val image = ImageProvider.fromResource(this.applicationContext, R.drawable.pin)
+        val mapObjects = mapView.map.mapObjects.addCollection()
+        val placeMark = mapObjects.addPlacemark(Point(55.994446, 92.797586), image)
+        val placeMark2 = mapObjects.addPlacemark(Point(56.008465, 92.766513), image)
+        val placeMark3 = mapObjects.addPlacemark(Point(53.723288, 91.449897), image)
+        placeMark.isVisible = true
+        placeMark.isDraggable = false
+        placeMark2.isVisible = true
+        placeMark2.isDraggable = false
+        placeMark3.isVisible = true
+        placeMark3.isDraggable = false
+
+        mapObjects.addTapListener()
+
+        placeMark.addTapListener{ _, _ ->
+            // Обработка нажатия на маркер
+            val position = placeMark.geometry
+            val message = "\nLatitude: ${position.latitude}, Longitude: ${position.longitude}"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            Toast.makeText(getApplicationContext() , "Its a toast!", Toast.LENGTH_SHORT).show()
+            true // Возвращаем true, чтобы остановить всплывающее окно кластера
+        }
+
+
+//        placeMark2.addTapListener{ _, _ ->
+//            // Обработка нажатия на маркер
+//            val position = placeMark.geometry
+//            val message = "Общежитие\nLatitude: ${position.latitude}, Longitude: ${position.longitude}"
+//            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//            print(123)
+//            true // Возвращаем true, чтобы остановить всплывающее окно кластера
+//        }
+//
+//        placeMark3.addTapListener{ _, _ ->
+//            // Обработка нажатия на маркер
+//            val position = placeMark.geometry
+//            val message = "Дом Родной\nLatitude: ${position.latitude}, Longitude: ${position.longitude}"
+//            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//            print(123)
+//            true // Возвращаем true, чтобы остановить всплывающее окно кластера
+//        }
+
 
 
         val btnBack = findViewById<Button>(R.id.btnBack)
@@ -41,4 +98,16 @@ class Map : AppCompatActivity() {
         MapKitFactory.getInstance().onStop()
         super.onStop    ()
     }
+
+    override fun onDrivingRoutes(p0: MutableList<DrivingRoute>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDrivingRoutesError(p0: Error) {
+        TODO("Not yet implemented")
+    }
+}
+
+private fun MapObjectCollection.addTapListener() {
+    println("НАЖАЛИ")
 }
--- app/src/main/java/com/forest/yoursystem/Timetable.kt
+++ app/src/main/java/com/forest/yoursystem/Timetable.kt
@@ -3,7 +3,10 @@ package com.forest.yoursystem
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+
 
 class Timetable : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,88 @@ class Timetable : AppCompatActivity() {
         btnBack.setOnClickListener {
             val startNewActivity = Intent(this, MainMenu::class.java)
             startActivity(startNewActivity)
+        }
+
+    }
+    fun onClick(v: View) {
+        val textView = findViewById<TextView>(R.id.TV_TimeTable)
+        when (v.id) {
+            R.id.btnMn -> textView.text = "Понедельник\n" +
+                    "Системы аналитической обработки данных\n" +
+                    "пр. занятие\n" +
+                    "синхронно\n" +
+                    "14:10–15:45\n" +
+                    "Корпус №17, ауд. 4-20\n" +
+                    "Якунин Ю. Ю.\n" +
+                    "Системы аналитической обработки данных\n" +
+                    "лекция\n" +
+                    "синхронно\n" +
+                    "15:55-17:30\n" +
+                    "Корпус №17, ауд. 5-15\n" +
+                    "Якунин Ю. Ю.\n" +
+                    "Экономическая культура и финансовая грамотность\n" +
+                    "пр. занятие\n" +
+                    "синхронно\n" +
+                    "17:40–19:15\n" +
+                    "Корпус №17, ауд. 2-20\n" +
+                    "Зябликов Д.В.\n"
+            R.id.btnTu -> textView.text = "Вторник\n" +
+                    "День военной подготовки\n" +
+                    "синхронно\n" +
+                    "08:30–10:05\n" +
+                    "День военной подготовки\n" +
+                    "синхронно\n" +
+                    "10:15–11:50\n" +
+                    "День военной подготовки\n" +
+                    "синхронно\n" +
+                    "12:00–13:35\n" +
+                    "День военной подготовки\n" +
+                    "синхронно\n" +
+                    "14:10–15:45\n" +
+                    "День военной подготовки\n" +
+                    "синхронно\n" +
+                    "15:55–17:30\n" +
+                    "День военной подготовки\n" +
+                    "синхронно\n" +
+                    "17:40–19:15"
+            R.id.btnWe -> textView.text = "Среда\n" +
+                    "Системы аналитической обработки данных\n" +
+                    "пр. занятие\n" +
+                    "синхронно\n" +
+                    "14:10–15:45\n" +
+                    "Корпус №17, ауд. 4-13\n" +
+                    "Якунин Ю. Ю.\n" +
+                    "Экономическая культура и финансовая грамотность\n" +
+                    "пр. занятие\n" +
+                    "синхронно\n" +
+                    "15:55–17:30\n" +
+                    "Корпус №17, ауд. 2-20\n" +
+                    "Зябликов Д. В.\n" +
+                    "Экономическая культура и финансовая грамотность\n" +
+                    "лекция\n" +
+                    "синхронно\n" +
+                    "17:40–19:15\n" +
+                    "Корпус №17, ауд. 1-15\n" +
+                    "Зябликов Д. В."
+            R.id.btnTh -> textView.text = "Четверг\n" +
+                    "Командный курсовой проект\n" +
+                    "пр. занятие\n" +
+                    "асинхронно\n" +
+                    "12:00–13:35\n" +
+                    "ЭИОС\n" +
+                    "Хныкин А. В.\n" +
+                    "Командный курсовой проект\n" +
+                    "пр. занятие\n" +
+                    "асинхронно\n" +
+                    "14:10–15:45\n" +
+                    "ЭИОС\n" +
+                    "Хныкин А. В."
+            R.id.btnFr -> textView.text = "Пятница\n" +
+                    " Занятий нет"
+            R.id.btnSa -> textView.text = "Суббота\n" +
+                    " Занятий нет"
+            R.id.btnSu -> textView.text = "Восресенье\n" +
+                    " Занятий нет"
         }
     }
 }
